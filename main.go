b0fb1d16abbd8f19d7a6b0e45673e91a2a07f2ca
Stheven Erlangga Sanjaya <sthevenerlanggasanjaya@gmail.com>
2022-03-12 19:18:18 +0700

    fifth module

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package module
 
 import (
 	"fmt"
-	"time"
 )
 
 type Verifikasi func(string, string) bool
@@ -14,80 +13,12 @@ type Author struct {
 var belanjaChannel = make(chan string)
 var membayarChannel = make(chan string)
 
-func main() {
-	authorAccess := Author{
-		Nama:     "stheven",
-		Password: "123456",
-	}
-
-	blokirAkses := func(nama, password string) bool {
-		return nama != "stheven"
-	}
-
-	VerifikasiAkses(authorAccess.Nama, authorAccess.Password, blokirAkses)
-
-	if authorAccess.Nama == "stheven" && authorAccess.Password == "123456" {
-		fmt.Print("\nActivity:\n1.Belajar\n2.Melakukan Konversi\n3.Berbelanja\n")
-		fmt.Print("Enter Option Activity: ")
-		var OptionActivity int
-		fmt.Scan(&OptionActivity)
-		fmt.Println("")
-
-		if OptionActivity == 1 {
-			Belajar()
-		} else if OptionActivity == 2 {
-			fmt.Print("Convert:\n1.Convert Feet to Meter\n2.Convert Mile to KM\n3.Convert Liter to CC\n")
-			fmt.Print("Enter Option Convert: ")
-			var OptionConvert int
-			fmt.Scan(&OptionConvert)
-			fmt.Println("")
-
-			if OptionConvert == 1 {
-				fmt.Print("Enter Feet: ")
-				var feet float64
-				fmt.Scan(&feet)
-				meter := FeetToMeter(feet)
-				fmt.Printf("'Hasil Konversi %.2f Feet adalah %.2f Meter'\n", feet, meter)
-			} else if OptionConvert == 2 {
-				fmt.Print("Enter Mile: ")
-				var Mile float64
-				fmt.Scan(&Mile)
-				km := MileToKM(Mile)
-				fmt.Printf("'Hasil Konversi %.2f Mile adalah %.2f KM\n'", Mile, km)
-			} else if OptionConvert == 3 {
-				fmt.Print("Enter CC: ")
-				var CC float64
-				fmt.Scan(&CC)
-				liter := CCToLiter(CC)
-				fmt.Printf("'Hasil Konversi %.1f CC adalah %.2f Liter'\n", CC, liter)
-			} else {
-				fmt.Println("Masukkan pilihan konversi anda dengan benar!")
-			}
-		} else if OptionActivity == 3 {
-			go Belanja()
-			time.Sleep(1 * time.Millisecond)
-			go Membayar()
-			time.Sleep(3 * time.Millisecond)
-			go SelesaiBelanja()
-			time.Sleep(5 * time.Millisecond)
-		} else {
-			fmt.Println("Masukkan pilihan aktivitas anda dengan benar!")
-		}
-	} else if authorAccess.Nama == "stheven" && authorAccess.Password != "123456" {
-		fmt.Println("'Masukkan password dengan benar!'")
-	} else if authorAccess.Nama != "stheven" && authorAccess.Password == "123456" {
-		fmt.Println("Masukkan nama dengan benar!'")
-	} else {
-		fmt.Print("Anda tidak memiliki akses!'")
-	}
-}
-
-func VerifikasiAkses(name string, password string, verifikasi Verifikasi) {
+func (author Author) VerifikasiAkses(name string, password string, verifikasi Verifikasi) {
 	if verifikasi(name, password) {
 		fmt.Print("'Pesan: ")
 	} else {
 		fmt.Print("'Pesan: ")
-		fmt.Printf("Selamat datang %s!'\n", name)
+		fmt.Printf("Selamat datang %s!'\n", author.Nama)
 	}
 }
 
@@ -178,5 +109,71 @@ func Membayar() {
 func SelesaiBelanja() {
 	for finished := range membayarChannel {
 		fmt.Println("Telah selesai belanja", finished)
+	}
+}
+
+func Matematika() {
+	channelKeliling := make(chan float64)
+	channelLuas := make(chan float64)
+	fmt.Print("\nMath:\n1.Lingkaran\n2.Balok\n3.Persegi\n")
+	fmt.Print("Enter Math: ")
+	var OptionMath int
+	fmt.Scan(&OptionMath)
+	fmt.Println("")
+
+	if OptionMath == 1 {
+		go func() (<-chan float64, <-chan float64) {
+			fmt.Print("Masukkan nilai r: ")
+			var r float64
+			fmt.Scan(&r)
+			luas := 3.14 * r * r
+			keliling := 3.14 * 2 * r
+			channelLuas <- luas
+			channelKeliling <- keliling
+			return channelLuas, channelKeliling
+		}()
+		luas := <-channelLuas
+		keliling := <-channelKeliling
+		fmt.Printf("luas lingkaran: %.2f\n", luas)
+		fmt.Printf("keliling lingkaran: %.2f\n", keliling)
+	} else if OptionMath == 2 {
+		go func() (<-chan float64, <-chan float64) {
+			fmt.Print("Masukkan nilai p: ")
+			var p float64
+			fmt.Scan(&p)
+			fmt.Print("Masukkan nilai l: ")
+			var l float64
+			fmt.Scan(&l)
+			fmt.Print("Masukkan nilai t: ")
+			var t float64
+			fmt.Scan(&t)
+			luas := 2 * (p*l + p*t + l*t)
+			keliling := 4 * (p + l + t)
+			channelLuas <- luas
+			channelKeliling <- keliling
+			return channelLuas, channelKeliling
+		}()
+		luas := <-channelLuas
+		keliling := <-channelKeliling
+		fmt.Printf("luas balok: %.2f\n", luas)
+		fmt.Printf("keliling balok: %.2f\n", keliling)
+	} else if OptionMath == 3 {
+		go func() (<-chan float64, <-chan float64) {
+			fmt.Print("Masukkan nilai s: ")
+			var s float64
+			fmt.Scan(&s)
+			luas := s * s
+			keliling := 4 * s
+			channelLuas <- luas
+			channelKeliling <- keliling
+			return channelLuas, channelKeliling
+		}()
+		luas := <-channelLuas
+		keliling := <-channelKeliling
+		fmt.Printf("luas persegi: %.2f\n", luas)
+		fmt.Printf("keliling persegi: %.2f\n", keliling)
+
+	} else {
+		fmt.Println("Masukkan pilihan matematika anda dengan benar!")
 	}
 }
